93e1115ca0f8e84f486511165864b4544c79ca00
jan <emperor_time300@icloud.com>
2025-03-17 12:51:53 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    // $('.p-headeSr__title').css('color','red');
+
+    $('.js-open--sidebar').click(function(){
+        $('.l-sidebar').toggleClass('is-active');
+        $('.c-sidebar-bg').toggleClass('is-active');
+    });
+});
